e422469bfd7544af2078cf29e2612a0f126692b1
abodi massarweh <abodi.msarwi@gmail.com>
2020-01-07 22:25:00 +0200

    final commit

--- src/Tests/Graph_AlgoTest.java
+++ src/Tests/Graph_AlgoTest.java
@@ -2,6 +2,9 @@ package Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,7 +14,9 @@ import org.junit.jupiter.api.Test;
 import algorithms.Graph_Algo;
 import dataStructure.DGraph;
 import dataStructure.NodeData;
+import dataStructure.node_data;
 import utils.Point3D;
+
 class Graph_AlgoTest {
 	Graph_Algo x;
 	DGraph y;
@@ -22,22 +27,7 @@ class Graph_AlgoTest {
 	@BeforeEach
 	void init_graph() {
 		x= new Graph_Algo();
-		DGraph y= new DGraph();
-		NodeData n0= new NodeData(new Point3D(0, 10));
-		NodeData n1= new NodeData(new Point3D(10, 10));
-		NodeData n2= new NodeData(new Point3D(10, 0));
-		NodeData n3= new NodeData(new Point3D(0, 0));
-		y.addNode(n0);
-		y.addNode(n1);
-		y.addNode(n2);
-		y.addNode(n3);
-		y.connect(0, 1, 700);
-		y.connect(1, 2, 10);
-		y.connect(2, 3, 12);
-		y.connect(3, 0, 500);
-		y.connect(0, 3, 0.5);
-		y.connect(3, 1, 0.7);
-		x.init(y);
+		y= new DGraph();
 	}
 	@AfterEach
 	void cleanup() {
@@ -54,53 +44,97 @@ class Graph_AlgoTest {
 		assertTrue(x.graph.equals(y));
 	}
 
-//	@Test
-//	void testInitString() {
-//		assertDoesNotThrow(()->x.init("Test.JSON"));
-//	}
+	@Test
+	void testInitString() {
+		assertDoesNotThrow(()->x.init("Test.txt"));
+	}
 
 	@Test
 	void testSave() {
 		x.init(y);
-		assertDoesNotThrow(()->x.save("Test.JSON"));
+		assertDoesNotThrow(()->x.save("Test.txt"));
 	}
 
 	@Test
 	void testIsConnected() {
 		x.init(y);
 		assertTrue(x.isConnected());
-		
+
 	}
 	@Test
 	void testShortestPathDist() {
-		assertEquals(0.5+0.7, x.shortestPathDist(0, 1));
+		NodeData n0= new NodeData(new Point3D(0, 10));
+		NodeData n1= new NodeData(new Point3D(10, 10));
+		NodeData n2= new NodeData(new Point3D(10, 0));
+		NodeData n3= new NodeData(new Point3D(0, 0));
+		y.addNode(n0);
+		y.addNode(n1);
+		y.addNode(n2);
+		y.addNode(n3);
+		y.connect(0, 1, 700);
+		y.connect(1, 2, 10);
+		y.connect(2, 3, 12);
+		y.connect(3, 0, 500);
+		y.connect(0, 3, 0.5);
+		y.connect(3, 1, 0.7);
+		x.init(y);
+		assertEquals(10.7, x.shortestPathDist(3, 2));
 	}
 
 	@Test
 	void testShortestPath() {
-		fail("Not yet implemented");
-	}
+		NodeData n0= new NodeData(new Point3D(0, 10));
+		NodeData n1= new NodeData(new Point3D(10, 10));
+		NodeData n2= new NodeData(new Point3D(10, 0));
+		NodeData n3= new NodeData(new Point3D(0, 0));
+		y.addNode(n0);
+		y.addNode(n1);
+		y.addNode(n2);
+		y.addNode(n3);
+		y.connect(0, 1, 700);
+		y.connect(1, 2, 10);
+		y.connect(2, 3, 12);
+		y.connect(3, 0, 500);
+		y.connect(0, 3, 0.5);
+		y.connect(3, 1, 0.7);
+		x.init(y);
 
+		List<node_data> real= x.shortestPath(3, 2);
+		List<node_data> expected= new ArrayList<node_data>();
+		expected.add(n3);
+		expected.add(n1);
+		expected.add(n2);
+		String s= real.toString();
+
+		assertTrue(expected.containsAll(real));
+
+	}
 	@Test
 	void testTSP() {
-		fail("Not yet implemented");
-	}
+		NodeData n0= new NodeData(new Point3D(0, 10));
+		NodeData n1= new NodeData(new Point3D(10, 10));
+		NodeData n2= new NodeData(new Point3D(10, 0));
+		NodeData n3= new NodeData(new Point3D(0, 0));
+		y.addNode(n0);
+		y.addNode(n1);
+		y.addNode(n2);
+		y.addNode(n3);
+		y.connect(0, 1, 700);
+		y.connect(1, 2, 10);
+		y.connect(2, 3, 12);
+		y.connect(3, 0, 500);
+		y.connect(0, 3, 0.5);
+		y.connect(3, 1, 0.7);
+		x.init(y);
+		List<node_data> real= x.shortestPath(3, 2);
+		List<node_data> expected= new ArrayList<node_data>();
+		expected.add(n3);
+		expected.add(n1);
+		expected.add(n2);
+		String s= real.toString();
 
+		assertTrue(expected.containsAll(real));
 
-
-	@Test
-	void testCopy() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testRelaxation() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testMain() {
-		fail("Not yet implemented");
 	}
 
 }
